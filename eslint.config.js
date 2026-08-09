e6b212f97a6ff95e48ca6c7346aea946e39892de
solarissongcontest <thesongcontest.tsc@gmail.com>
2026-08-09 21:13:46 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -6,20 +6,36 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", ".output", ".vinxi"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: [
+      "dist",
+      ".output",
+      ".vinxi",
+      "src/routeTree.gen.ts",
+    ],
+  },
+
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+
     files: ["**/*.{ts,tsx}"],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+
     rules: {
       ...reactHooks.configs.recommended.rules,
+
       "no-restricted-imports": [
         "error",
         {
@@ -32,9 +48,30 @@ export default tseslint.config(
           ],
         },
       ],
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
+
+  /*
+   * Keep Prettier integrated with ESLint, but do not allow
+   * formatting differences to make the application fail CI.
+   *
+   * Formatting is useful.
+   * Formatting preventing a preview from existing is rather less useful.
+   */
   eslintPluginPrettier,
+
+  {
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
 );
