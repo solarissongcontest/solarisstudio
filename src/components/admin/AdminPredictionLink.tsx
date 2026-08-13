2b6e25d9765de90f229795cb9c216b5459c7a4bb
solarissongcontest <thesongcontest.tsc@gmail.com>
2026-08-13 18:07:36 +0300

    Add prediction navigation item

---
+++ src/components/admin/AdminPredictionLink.tsx
@@ -0,0 +1,2 @@
+import { Link } from "@tanstack/react-router";
+export const AdminPredictionLink = () => <Link to="/admin/predictions" className="block rounded-lg px-3 py-2 hover:bg-surface">Predictions</Link>;
