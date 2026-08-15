cb37946c56a0c5cd58d97486dc76a7bc9dc9e320
solarissongcontest <thesongcontest.tsc@gmail.com>
2026-08-15 16:13:01 +0300

    Use named route option identifiers for TanStack splitter

--- src/routes/_authenticated/admin/$slug.tsx
+++ src/routes/_authenticated/admin/$slug.tsx
@@ -3,8 +3,10 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Route as EditionFeatureRoute } from "@/features/admin/edition/AdminEditionRoute";
 
 const featureOptions = EditionFeatureRoute.options as any;
+const AdminEditionComponent = featureOptions.component;
+const editionHead = featureOptions.head;
 
 export const Route = createFileRoute("/_authenticated/admin/$slug")({
-  component: featureOptions.component,
-  head: featureOptions.head,
+  component: AdminEditionComponent,
+  head: editionHead,
 });
