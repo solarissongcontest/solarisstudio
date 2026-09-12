98c91a9a2e21cc653ed3de46efa01b3a56bc8fa2
solarissongcontest <thesongcontest.tsc@gmail.com>
2026-09-12 19:59:22 +0300

    Expose modular SSC rulebook public API

---
+++ src/lib/ssc-rules/index.ts
@@ -0,0 +1,5 @@
+export * from "@/lib/ssc-rules/types";
+export * from "@/lib/ssc-rules/canon-v4";
+export * from "@/lib/ssc-rules/aliases";
+export * from "@/lib/ssc-rules/search";
+export * from "@/lib/ssc-rules/runtime-overlay";
